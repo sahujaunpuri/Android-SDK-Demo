cb0b30c1839959b88e038d5b734eb824f9672a80
Thomas So <thomas.m.so33@gmail.com>
2018-12-01 15:08:02 -0800

    Remove references to AppLovin dashboard toggles

--- Android-Demo-App/src/main/java/com/applovin/apps/demoapp/nativeads/NativeAdDemoMenuActivity.java
+++ Android-Demo-App/src/main/java/com/applovin/apps/demoapp/nativeads/NativeAdDemoMenuActivity.java
@@ -1,10 +1,6 @@
 package com.applovin.apps.demoapp.nativeads;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.text.Html;
-import android.view.Gravity;
-import android.widget.TextView;
 
 import com.applovin.apps.demoapp.DemoMenuActivity;
 import com.applovin.apps.demoapp.DemoMenuItem;
@@ -12,21 +8,6 @@ import com.applovin.apps.demoapp.DemoMenuItem;
 public class NativeAdDemoMenuActivity
         extends DemoMenuActivity
 {
-
-    @Override
-    protected void setupListViewFooter()
-    {
-        TextView footer = new TextView( getApplicationContext() );
-        footer.setPadding( 0, 20, 0, 0 );
-        footer.setGravity( Gravity.CENTER );
-        footer.setTextSize( 18 );
-        footer.setTextColor( Color.RED );
-        footer.setText( Html.fromHtml( "You must turn <b>ON</b> \" Native Ads\" in your AppLovin dashboard" ) );
-
-        listView.addFooterView( footer );
-        listView.setFooterDividersEnabled( false );
-    }
-
     @Override
     protected DemoMenuItem[] getListViewContents()
     {
--- Android-Kotlin-Demo-App/src/main/java/com/applovin/apps/kotlindemoapp/nativeads/NativeAdDemoMenuActivity.kt
+++ Android-Kotlin-Demo-App/src/main/java/com/applovin/apps/kotlindemoapp/nativeads/NativeAdDemoMenuActivity.kt
@@ -4,8 +4,7 @@ import android.content.Intent
 import com.applovin.apps.kotlindemoapp.DemoMenuActivity
 import com.applovin.apps.kotlindemoapp.DemoMenuItem
 
-class NativeAdDemoMenuActivity : DemoMenuActivity()
-{
+class NativeAdDemoMenuActivity : DemoMenuActivity() {
     override fun getListViewContents(): Array<DemoMenuItem> = arrayOf(
             DemoMenuItem("Single ad", "Programmatically loading an ad using our open-source carousel view", Intent(this, NativeAdCarouselUIActivity::class.java)),
             DemoMenuItem("Multiple ads", "Simple native ads in a RecyclerView", Intent(this, NativeAdRecyclerViewActivity::class.java))
